d8b9df1d9ae10ce4d22d4acc583a794c51e4ee27
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-30 12:37:23 +0300

    findViewById

--- FindById/app/src/main/java/com/sum/findviewbyid/MainActivity.kt
+++ FindById/app/src/main/java/com/sum/findviewbyid/MainActivity.kt
@@ -2,14 +2,32 @@ package com.sum.findviewbyid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = "İzmir"
-        textView2.text ="Bornava"
+        val textName:TextView = findViewById(R.id.textView)
+        val textPopulation:TextView = findViewById(R.id.textView2)
+        val textFamous:TextView = findViewById(R.id.textView3)
+        val buttonClick: Button = findViewById(R.id.btnClick)
+
+        val izmir = City("İzmir",3000,"Boyoz")
+
+        textName.text = izmir.name
+        textPopulation.text = "Nüfusu: ${izmir.population}"
+        textFamous.text =izmir.famousWith
+
+        buttonClick.setOnClickListener {
+            Toast.makeText(applicationContext,"Button Cliked",Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
