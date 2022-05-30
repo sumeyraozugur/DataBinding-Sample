9811270e6e6453b3b8b23d06b68a99d33532cab7
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-30 11:16:10 +0300

    databinding button added

--- DataBinding/app/src/main/java/com/sum/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/sum/databinding/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sum.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.sum.databinding.databinding.ActivityMainBinding
 
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.city =City("İzmir",300,"Boyoz")
+
+        binding.btnClick.setOnClickListener {
+            Toast.makeText(applicationContext, "Button cliked",Toast.LENGTH_SHORT).show()
+        }
 
 
 
