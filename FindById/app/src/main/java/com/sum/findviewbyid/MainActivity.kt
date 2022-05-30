46f6c3109acc48a6a3204f7a8897a5b643b7bc50
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-30 11:57:27 +0300

    Kotlin synthetic

---
+++ FindById/app/src/main/java/com/sum/findviewbyid/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sum.findviewbyid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "İzmir"
+        textView2.text ="Bornava"
+    }
+}
